e69037916e382fc52da92ab9b0eea36623e7cb99
ridhima-2412 <ridhimanagpal2412@gmail.com>
2026-04-18 15:02:01 +0530

    Add Navbar component with navigation functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,8 @@
-
+export default function Navbar({ setPage }) {
+  return (
+    <nav className="navbar">
+      <h2 onClick={() => setPage("home")}>AttentionX</h2>
+      <button onClick={() => setPage("upload")}>Get Started</button>
+    </nav>
+  );
+}
