97586fdca3592b6d8e3244108b184b7ed49c679a
ridhima-2412 <ridhimanagpal2412@gmail.com>
2026-04-18 15:07:24 +0530

    Update Results.jsx

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,1 +1,19 @@
+export default function Results({ setPage }) {
+  return (
+    <div className="page">
+      <h1>Generated Clips</h1>
 
+      <div className="card">
+        <h3>Clip 1</h3>
+        <p>Motivation Peak - 00:32</p>
+      </div>
+
+      <div className="card">
+        <h3>Clip 2</h3>
+        <p>Funny Moment - 12:10</p>
+      </div>
+
+      <button onClick={() => setPage("export")}>Export Reel</button>
+    </div>
+  );
+}
